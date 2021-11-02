d77d9bf76cfe6577e4d4844220ccbf51e8f0421a
Marco <sonni46marcosenpai@gmail.com>
2021-11-02 19:27:41 +0100

    bonus

--- laravel-base-crud/resources/views/comics/show.blade.php
+++ laravel-base-crud/resources/views/comics/show.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-sono show
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>{{ $comic['title'] }}</h1>
+            <ul>
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                {{-- i punti esclamativi servono per visualizare meglio certi caratteri nel db --}}
+                <li>{!! $comic['description'] !!}</li>
+                <li>{{ $comic['type'] }}</li>
+                <li>{{ $comic['price'] }}</li>
+            </ul>
+        </div>
+    </div>
+</div>
 @endsection
